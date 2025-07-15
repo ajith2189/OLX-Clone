03054b7cd3ca0f12fe5e786fae24a1e4241cdf9b
Ajith <130132590+ajith2189@users.noreply.github.com>
2025-07-15 09:29:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Pages/Home'
 import Details from './Components/Details/Details'
 
-// creating app  for the
+// creating app  for the some of teh
 const App = () => {
   
   return (
